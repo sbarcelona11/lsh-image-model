91df03b881db86996d884d104a1b33c6242cc481
Sebastian Barcelona <sbarcelona@gmail.com>
2023-08-11 18:07:04 -0300

    add image_str returned

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import torch
 from potassium import Potassium, Request, Response
 from diffusers import DiffusionPipeline
+from io import BytesIO
+import base64
 
 app = Potassium("my_app")
 
@@ -23,10 +25,23 @@ def init():
 def handler(context: dict, request: Request) -> Response:
     prompt = request.json.get("prompt")
     model = context.get("model")
-    outputs = model(prompt, num_images_per_prompt=1, height=512, width=512).images
+    negative_prompt = "(worst quality, low quality:1.4), monochrome, zombie, (interlocked fingers), cleavage, nudity, naked, nude"
+    image = model(
+        prompt=prompt,
+        negative_prompt=negative_prompt,
+        guidance_scale=7,
+        num_inference_steps=request.json.get("steps", 30),
+        generator=torch.Generator(device="cuda").manual_seed(request.json.get("seed")) if request.json.get("seed") else None,
+        width=512,
+        height=512,
+    ).images[0]
+
+    buffered = BytesIO()
+    image.save(buffered, format="JPEG", quality=80)
+    img_str = base64.b64encode(buffered.getvalue())
 
     return Response(
-        json = {"outputs": outputs[0]}, 
+        json = {"output": str(img_str, "utf-8")},
         status=200
     )
 

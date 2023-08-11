272ca974c5472517f939f0539d7ebf7ea45f9c44
Sebastian Barcelona <sbarcelona@gmail.com>
2023-08-11 18:54:35 -0300

    more changes to review better functionality

--- download.py
+++ download.py
@@ -1,11 +1,11 @@
 # This file runs during container build time to get model weights built into the container
 # In this example: A Huggingface BERT model
-from diffusers import DiffusionPipeline
+from diffusers import StableDiffusionPipeline
 
 
 def download_model():
     model_id = "sbarcelona11/KIDS-ILLUSTRATION-LSH"
-    DiffusionPipeline.from_pretrained(model_id, use_safetensors=True, safety_checker=None)
+    StableDiffusionPipeline.from_pretrained(model_id, use_safetensors=True, safety_checker=None)
     print("Model downloaded")
 
 

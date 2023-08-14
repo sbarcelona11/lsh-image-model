9d597c0355553de479c1b967aa035a6bb7d2d954
Sebastian Barcelona <sbarcelona@gmail.com>
2023-08-14 15:18:48 -0300

    add more negative prompts and some changes to the model work better

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
 import torch
 from potassium import Potassium, Request, Response
-from diffusers import StableDiffusionPipeline, DiffusionPipeline, DPMSolverMultistepScheduler
+from diffusers import DiffusionPipeline
 from io import BytesIO
 import base64
 
 app = Potassium("my_app")
+
 
 # @app.init runs at startup, and loads models into the app's context
 @app.init
 def init():
     device = 0 if torch.cuda.is_available() else -1
     repo_id = "sbarcelona11/KIDS-ILLUSTRATION-LSH"
-    # ddpm = DPMSolverMultistepScheduler.from_pretrained(repo_id, subfolder="scheduler")
     model = DiffusionPipeline.from_pretrained(
         repo_id,
         use_safetensors=True,
         torch_dtype=torch.float16,
-        # scheduler=ddpm
     ).to("cuda")
-    # model = StableDiffusionPipeline.from_pretrained(repo_id, use_safetensors=True)
     model.to(device)
 
     context = {
@@ -27,17 +25,21 @@ def init():
 
     return context
 
+
 # @app.handler runs for every call
 @app.handler("/")
 def handler(context: dict, request: Request) -> Response:
     prompt = request.json.get("prompt")
     model = context.get("model")
-    negative_prompt = "(worst quality, low quality:1.4), monochrome, zombie, (interlocked fingers), cleavage, nudity, naked, nude"
+    negative_prompt = ("(worst quality, low quality:1.4), monochrome, zombie, (interlocked fingers), cleavage, nudity, "
+                       "naked, nude, amputee, bad anatomy, bad proportions, bad composition, deformed, extra arms, "
+                       "extra fingers, extra hands, extra legs, missing arms, missing fingers, missing hands, "
+                       "missing legs, mutated hands, mutated legs, mutation, mutilated hands, mutilated legs")
     image = model(
         prompt=prompt,
         negative_prompt=negative_prompt,
         guidance_scale=7,
-        num_inference_steps=request.json.get("steps", 50),
+        num_inference_steps=request.json.get("steps", 30),
         width=512,
         height=512,
     ).images[0]
@@ -47,9 +49,10 @@ def handler(context: dict, request: Request) -> Response:
     img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
 
     return Response(
-        json = {"output": img_str},
+        json={"output": img_str},
         status=200
     )
+
 
 if __name__ == "__main__":
     app.serve()
